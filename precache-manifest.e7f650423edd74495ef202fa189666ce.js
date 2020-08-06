34ab33bf285b332c1c9cf55be8ae1825abbd23d5
Abhishek <mailabhishekdutta@gmail.com>
2020-08-06 17:46:53 +0530

    Updates

---
+++ precache-manifest.e7f650423edd74495ef202fa189666ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fcaa6192609848adedffebc4ee7bc09",
+    "url": "/EcommerceReact/index.html"
+  },
+  {
+    "revision": "064140d342cb787762ab",
+    "url": "/EcommerceReact/static/css/main.97a07a86.chunk.css"
+  },
+  {
+    "revision": "b1d47a9d914b3727ef07",
+    "url": "/EcommerceReact/static/js/2.1df1248c.chunk.js"
+  },
+  {
+    "revision": "064140d342cb787762ab",
+    "url": "/EcommerceReact/static/js/main.b067fe76.chunk.js"
+  },
+  {
+    "revision": "2eadd44e8cd4ca1fcfc1",
+    "url": "/EcommerceReact/static/js/runtime-main.c1b7328b.js"
+  },
+  {
+    "revision": "8894b7d37bd41e50b31318e97d50b323",
+    "url": "/EcommerceReact/static/media/loading.8894b7d3.gif"
+  },
+  {
+    "revision": "7ac3ba3ab8503352edfc941d7ecd430d",
+    "url": "/EcommerceReact/static/media/logo.7ac3ba3a.svg"
+  },
+  {
+    "revision": "016d4fcc158e863ea29afc6b6ff6c3ff",
+    "url": "/EcommerceReact/static/media/mainBcg.016d4fcc.jpeg"
+  }
+]);
